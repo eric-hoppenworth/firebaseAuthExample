22017cf41517da8d736bc2de2d4249d05bca8566
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-12-15 12:22:45 -0500

    initial commit

---
+++ assets/js/firebase.js
@@ -0,0 +1,54 @@
+  // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBBBlmMQd_X2gGZsb_OtMJNvIaYGRDCVBM",
+  authDomain: "authexample-e326d.firebaseapp.com",
+  databaseURL: "https://authexample-e326d.firebaseio.com",
+  projectId: "authexample-e326d",
+  storageBucket: "authexample-e326d.appspot.com",
+  messagingSenderId: "945928826461"
+};
+firebase.initializeApp(config);
+var usersEndPoint = firebase.database.ref("/Users");
+var firebaseUser;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+	// User is signed in.
+	usersEndPoint.once("value",function(snapshot){
+	  firebaseUser = snapshot.val()[user.uid];
+	})
+  } else {
+	// No user is signed in.
+  }
+});
+
+$("#btnSignUp").on("click",function(event){
+  //prevent any default action
+  //in this example, the button has no default action, but best to be safe!
+  event.preventDefault();
+  var userName = $("#userNameInput").val();
+  var password = $("#passwordInput").val();
+  firebase.auth().createUserWithEmailAndPassword(userName, password)
+	.then(function(user){
+	  usersEndPoint.child(user.uid).set({"userName",user.email})
+	})
+	.catch(function(error) {
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	// ...
+  });
+})
+
+$("#btnSignIn").on("click",function(event){
+  //prevent any default action
+  //in this example, the button has no default action, but best to be safe!
+  event.preventDefault();
+  var userName = $("#userNameInput").val();
+  var password = $("#passwordInput").val();
+  firebase.auth().signInWithEmailAndPassword(userName, password).catch(function(error) {
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	// ...
+  });
+})
